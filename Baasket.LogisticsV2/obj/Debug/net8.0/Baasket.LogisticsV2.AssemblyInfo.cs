5778b25dc6a0d025fc0aa1c63c07648a0f954d97
lamasiam <lamapalestine8@gmail.com>
2026-06-17 00:27:25 +0800

    Observer pattern output file

--- Baasket.LogisticsV2/obj/Debug/net8.0/Baasket.LogisticsV2.AssemblyInfo.cs
+++ Baasket.LogisticsV2/obj/Debug/net8.0/Baasket.LogisticsV2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Baasket.LogisticsV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+338157966eda5cf4ebe94c73f095d8918c1cd7e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94a06277a1ebe6e905b21f101a7e9b92aa97d7a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Baasket.LogisticsV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Baasket.LogisticsV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
